68f692750a2a4449dcaec347116d29f2afc9b075
moosemaniam <vikasmk@gmail.com>
2017-04-12 08:48:35 +0530

    Deeper version of Lenet
    -Replaced the , issue with .
    -Validation loss is around 0.0155 after the very first EPOCH
     Might mean the data is not enough and the model is underfitting
    -Car does minor left turn modifications here and there and negotiates
     the first slow curve.
    -But, does not manage the first red curve
    -Plan is to do more augmentation

--- read_process_data.py
+++ read_process_data.py
@@ -1,7 +1,6 @@
 import csv
 import cv2
 import numpy as np
-limit=1000
 lines=[]
 with open('data/driving_log.csv') as csvfile:
   reader = csv.reader(csvfile,delimiter=',')
@@ -13,9 +12,7 @@ measurements=[]
 index=-1
 for line in lines:
   #Ignore first row which contains labels
-  if(index==limit):
-    continue
-  if(index == -1):
+    if(index == -1):
     index=0
     continue
 
@@ -24,10 +21,9 @@ for line in lines:
   filename = source_path.split('/')[-1]
   current_path = 'data/IMG/' + filename
   image = cv2.imread(current_path)
-  measurement = float(line[3])
+  measurement = float(line[3].replace(",","."))
 #Only take non zero index values
-  if(measurement!=0.0):
-   print(measurement)
+  if(1 or measurement!=0.0):
    image = cv2.cvtColor( image, cv2.COLOR_BGR2RGB)
    images.append(image)
    measurements.append(measurement)
